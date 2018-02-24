e1a86ddb0ab92a98109d5bcd21d045f0b9be4689
Ryohei Ueda <garaemon@gmail.com>
2018-02-24 15:06:11 +0900

    Create database before putting data

--- looper.sh
+++ looper.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+# create database
+curl -XPOST http://$INFLUXDB_HOST:$INFLUXDB_PORT/query \
+    -d "q=CREATE DATABASE $INFLUXDB_DATABASE"
+
 while [ 1 ]
 do
     echo "---> Starting new test"
